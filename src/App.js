93c077aa9f167ae26dbc1425947a070961ac22bc
wyhong3103 <wyhong3103@gmail.com>
2023-02-02 22:04:32 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LogIn } from './pages/LogIn';
+import { Menu } from './pages/Menu';
+import { Game } from './pages/Game';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => {
+    return(
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<LogIn/>}
+                    />
+                    <Route
+                        path="/login"
+                        element={<LogIn/>}
+                    />
+                    <Route
+                        path="/menu"
+                        element={<Menu/>}
+                    />
+                    <Route
+                        path="/game/:id"
+                        element={<Game/>}
+                    />
+                </Routes>    
+            </BrowserRouter>
+        </div>
+    )
+};

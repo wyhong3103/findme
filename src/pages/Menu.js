2663da09340cd139e878673821d61649dcf63e75
wyhong3103 <wyhong3103@gmail.com>
2023-02-03 15:45:23 +0800

    Add navigation from menu to game

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,18 +2,23 @@ import '../styles/Menu.css';
 import { MenuNav } from '../components/MenuNav';
 import {useContext} from 'react';
 import { UserContext } from '../context/UserContext';
-import { Navigate } from 'react-router-dom';
+import { useNavigate, Navigate } from 'react-router-dom';
 
 
 export const Menu = () => {
 
     const firstTime = useContext(UserContext).firstTime;
+    const navigate = useNavigate();
 
     const games = [
         [1, "Snow"],
         [2, "Space"],
         [3, "Beach"],
     ];
+
+    const toGame = (id) => {
+        navigate(`/game/${id}`);
+    }
 
     return(
         <div>
@@ -30,7 +35,7 @@ export const Menu = () => {
                         {
                                 games.map((item) => {
                                     return(
-                                        <div className="menu-card">
+                                        <div className="menu-card" onClick={() => {toGame(item[0])}}>
                                             <h3>{item[1]}</h3>
                                             <img src={require(`../assets/m${item[0]}.jpg`)} alt={item[1]} className="menu-card-img"/>
                                         </div>

d7a02af06fd1f53823fdcd966a5e156f8e25db1e
wyhong3103 <wyhong3103@gmail.com>
2023-02-03 15:36:10 +0800

    Add context to navigation

--- src/components/MenuNav.js
+++ src/components/MenuNav.js
@@ -1,12 +1,16 @@
 import '../styles/MenuNav.css';
+import {useContext} from 'react';
+import { UserContext } from '../context/UserContext';
 
 export const MenuNav = () => {
+    const context = useContext(UserContext);
+
     return(
         <div className="menu-nav">
             <h1 className="menu-nav-title">FindMe</h1>
             <div className="menu-nav-user">
-                <img src="https://st3.depositphotos.com/7486768/17806/v/600/depositphotos_178065822-stock-illustration-profile-anonymous-face-icon-gray.jpg" alt="profile-img"/>
-                <h3>Anonymous</h3>
+                <img src={context.pfp} alt="profile-img"/>
+                <h3>{context.username}</h3>
             </div>
         </div>
     )

da21c81233895af71a6e22bf7b5dd4774e3cf15f
wyhong3103 <wyhong3103@gmail.com>
2023-02-03 22:19:06 +0800

    Add dropdown menu & styles

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,21 +1,36 @@
 import '../styles/Game.css';
 import { useParams } from 'react-router-dom';
 import { GameNav } from '../components/GameNav';
-import {useContext} from 'react';
+import { useRef, useContext, useState } from 'react';
 import { UserContext } from '../context/UserContext';
 import { Navigate } from 'react-router-dom';
 
 export const Game = () => {
     
     const firstTime = useContext(UserContext).firstTime;
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+    const [hideDropdown, setHideDropDown] = useState(true);
     const {id} = useParams();
+    const dropdown = useRef();
+    const game = useRef();
 
     const bg = require(`../assets/${id}.jpg`);
 
     const objects = [
         ["Waldo", 1],
-        ["Wizard", 0]
+        ["Wizard", 1]
     ];
+
+    const setDropdown = (x, y) => {
+        setHideDropDown(prev => !prev);
+    }
+
+    const setCoordinate = (e) => {
+        setDropdown();
+        setX(Math.floor(e.nativeEvent.offsetX / e.nativeEvent.target.offsetWidth * 100));
+        setY(Math.floor(e.nativeEvent.offsetY / e.nativeEvent.target.offsetHeight * 100));
+    }
 
     return(
         <div>
@@ -28,8 +43,34 @@ export const Game = () => {
 
                 <div>
                     <GameNav objects={objects}/>
-                    <div className="game-img-cont">
-                        <img src={bg} alt="game-img" onClick={(e) => {console.log(e)}}/>
+                    <div class="game-cont">
+                        <div className="game-img-cont">
+                            <img src={bg} alt="game-img" onClick={setCoordinate} ref={game}/>
+                            <div
+                                className={`dropdown ${(hideDropdown ? "hide" : "show")}`}
+                                style= {
+                                    {
+                                        top : `${(y/100)*(game.current ? game.current.offsetHeight : 1)}px`,
+                                        left : `${(x/100)*(game.current ? game.current.offsetWidth : 1)}px`
+                                    }
+                                }
+                                ref={dropdown}>
+                                <ul>
+                                    {
+                                        objects.map((item) => {
+                                            return(
+                                                item[1] ?
+                                                <li>
+                                                    {item[0]}
+                                                </li>
+                                                :
+                                                null
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                    </div>
                     </div>
                 </div>
             }

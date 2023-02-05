f3ad96140707f08271ca7c98102214cf6560b2a1
wyhong3103 <wyhong3103@gmail.com>
2023-02-05 20:28:17 +0800

    Add firestore to get all the levels

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,24 +1,37 @@
 import '../styles/Menu.css';
 import { MenuNav } from '../components/MenuNav';
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import { UserContext } from '../context/UserContext';
 import { useNavigate, Navigate } from 'react-router-dom';
+import {getFirestore, collection, getDocs} from 'firebase/firestore';
 
 
 export const Menu = () => {
 
+    const db = getFirestore();
     const firstTime = useContext(UserContext).firstTime;
     const navigate = useNavigate();
-
-    const games = [
-        [1, "Snow"],
-        [2, "Space"],
-        [3, "Beach"],
-    ];
+    const [levels, setLevels] = useState([]);
 
     const toGame = (id) => {
         navigate(`/game/${id}`);
     }
+
+    useEffect(
+        () => {
+            (async () => {
+                const temp = [];
+                const levelsRef = collection(db, "game");
+                const snapshot = await getDocs(levelsRef);
+                snapshot.forEach(
+                    doc => {
+                        temp.push([doc.id, doc.data().levelName]);
+                    }
+                )
+                setLevels(temp);
+            })();
+        }
+    , [])
 
     return(
         <div>
@@ -33,7 +46,7 @@ export const Menu = () => {
                     <MenuNav/>
                     <div className="menu-card-cont">
                         {
-                                games.map((item) => {
+                                levels.map((item) => {
                                     return(
                                         <div className="menu-card" onClick={() => {toGame(item[0])}}>
                                             <h3>{item[1]}</h3>

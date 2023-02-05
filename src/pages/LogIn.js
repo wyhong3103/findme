aa923800faaf0d96b25b863e9eac5192177c4198
wyhong3103 <wyhong3103@gmail.com>
2023-02-05 19:36:39 +0800

    Add firebase auth

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,12 +1,16 @@
 import '../styles/LogIn.css';
+import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult } from 'firebase/auth';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export const LogIn = ({setFirstTime, setUsername, setPfp}) => {
 
     const navigate = useNavigate();
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
 
     const signInGoogle = () => {
-
+        signInWithRedirect(auth, provider);
     };
 
     const signInAnonymous = () => {
@@ -14,10 +18,31 @@ export const LogIn = ({setFirstTime, setUsername, setPfp}) => {
         navigate("/");
     };
 
+    useEffect(
+        () => { 
+            getRedirectResult(auth)
+            .then(
+                (result) => {
+                    if (result.user){
+                        setFirstTime(false);
+                        setUsername(result.user.displayName);
+                        setPfp(result.user.photoURL);
+                        navigate("/");
+                    }
+                }
+            )
+            .catch(
+                (error) => {
+                    console.error(error);
+                }
+            )
+        }
+    , [])
+
     return(
         <div className='login-cont'>
             <h1 className="login-title">FindMe</h1>
-            <button className="login-btn">
+            <button className="login-btn" onClick={signInGoogle}>
                 Sign In With Google
             </button>
             <button className="login-btn" onClick={signInAnonymous}>
